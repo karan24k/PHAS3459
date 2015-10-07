4b464dc2f281fe5a9de7bf14f2c0fc4f09d57f77
karankullar <jaskaran.kullar.13@ucl.ac.uk>
2015-10-07 12:13:36 +0100

    written the exact same string to print as someone else, doesn't work on my machine... peculiar

--- PHAS3459/src/module1/VectorMethods.java
+++ PHAS3459/src/module1/VectorMethods.java
@@ -1,23 +1,36 @@
 package module1;
+import java.lang.Math.*;
 
 public class VectorMethods {
-	public double dotproduct(double a) {
-		double x;
-		x = a*d + b*e + c*f;
-		return x;
-	}
-	
 	public static void main(String[] args) {
 		VectorMethods vm = new VectorMethods();
-		double a = 1.0;
-		double b = 2.0;
-		double c = 3.0;
-		double d = 4.0;
-		double e = 5.0;
-		double f = 6.0;
-		
-		double mag = vm.magnitude(a,b,c);
+		double prod = vm.dotprod(1.0, 2.0, 3.0, 4.0, 5.0, 6.0);
+		double mag = vm.magnitude(1.0, 2.0, 3.0);
+		double ang = vm.angle(2.0, 4.0, 1.0, 5.0, 3.0, 1.0);
 
 	}
+	
+	public double dotprod(double ax, double ay, double az, double bx, double by, double bz) {
+		double x;
+		x = ax*bx + ay*by + az*bz;
+		System.out.println("Vector A is " + "(" + ax +","  + ay +"," + az +") " + "and Vector B is "+ "(" + bx +","  + by +"," + bz +")");
+		System.out.println("The dot product between the two vectors is " + x);
+		return x;
+			
+	}
+	
+	public double magnitude(double ax, double ay, double az) {
+		double y;
+		y = Math.sqrt(ax*ax + ay*ay + az*az);
+		System.out.println("The magnitude of the vector (1.0,2.0,3.0) is " + y);
+		return y;
+	}
+	
+	public double angle(double ax, double ay, double az, double bx, double by, double bz) {
+		double z;
+		z = Math.acos(dotprod(ax, ay, az, bx, by, bz)/(magnitude(ax, ay, az)*magnitude(bx, by, bz)));
+		System.out.println(z);
+		return z;
 
+	}
 }
