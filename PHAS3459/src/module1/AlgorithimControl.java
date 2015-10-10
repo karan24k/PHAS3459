aeacebd9b42b8819b6ce9e919fdc378a9ef1ef06
karankullar <jaskaran.kullar.13@ucl.ac.uk>
2015-10-10 18:51:40 +0100

    not sure if i have written the methods properly

---
+++ PHAS3459/src/module1/AlgorithimControl.java
@@ -0,0 +1,32 @@
+package module1;
+
+public class AlgorithimControl {
+
+	public static void main(String[] args) {
+		int x = 1;
+		int xmax = 17;
+		for (x = 1 ; x < xmax; x++) {
+		System.out.println("x = " +x);
+		}
+		
+		{
+		System.out.print("\n");
+		}
+		
+		int y = 10;
+		int ymin = -5;
+
+		while( y >= ymin) {
+		System.out.print("y = " +y );
+		y--;
+		System.out.print("\n");
+		}
+		
+		{
+		System.out.print("\n");
+		}
+		
+		
+		}
+}
+		
