ac30da03889dfbd4118d63fa24b1ba24e36e91dc
karankullar <jaskaran.kullar.13@ucl.ac.uk>
2015-10-11 22:37:23 +0100

    commented

--- PHAS3459/src/module1/DataTypes.java
+++ PHAS3459/src/module1/DataTypes.java
@@ -3,27 +3,27 @@ package module1;
 public class DataTypes {
 
 	public static void main(String[] args) {
-		double doubVar = 10.0;
+		double doubVar = 10.0; //double sets the data type to a decimal with 64 bits storage (double precision)
 		System.out.println("The double value is " + doubVar);
 		System.out.println("The square is " + doubVar*doubVar + "\n");
 		
-		float floatVar = 10f;
+		float floatVar = 10f; //float is a decimal, 32 bits (single precision)
 		System.out.println("The float value is " + floatVar);
 		System.out.println("The square is " + floatVar*floatVar + "\n");
 		
-		int intVar = 10;
+		int intVar = 10; //int gives an integer
 		System.out.println("The integer value is " + intVar);
 		System.out.println("The square is " + intVar*intVar + "\n");
 		
-		long longVar = 10;
+		long longVar = 10; //64 bit integer
 		System.out.println("The long value is " + longVar);
 		System.out.println("The square is " + longVar*longVar + "\n");
 		
-		byte byteVar = 10;
+		byte byteVar = 10; // 8 bit integer
 		System.out.println("The byte value is " + byteVar);
 		System.out.println("The square is " + byteVar*byteVar + "\n");
 		
-		char charVar = 'a' + 10;
+		char charVar = 'a' + 10; //16 bit unicode character
 		System.out.println("The character output is " + charVar + "\n");
 		/*The output gives 10 letters along the alphabet from a
 		 *  (GENIUS)
@@ -36,12 +36,12 @@ public class DataTypes {
 		char charVar3 = 'z' + 1;
 		System.out.println("The character output is " + charVar3 + "\n");
 		/* Past letters we get non alphabetical characters
-		 * Uppercase characters come before lowercase
+		 * Uppercase characters come before lowercase this is because it goes through unicode
 		 */
 		
 		/* int j = 1; int i; j = i + 1;
 		 * System.out.println(j);
-		 * trying to say j is i + 1 when i isn't initialised is impossible
+		 * trying to say j is i + 1 when i isn't given a value (initialised) is impossible
 		 */
 		
 		double castTest = 4.99;
@@ -51,10 +51,6 @@ public class DataTypes {
 		 *  disregards anything  after a decimal point
 		 *  i.e. always rounds down
 		 */
-		
-		
-		
-
 	}
 
 }
