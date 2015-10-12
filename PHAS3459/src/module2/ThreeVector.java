30714a553a44e6de89c11a8edcb2f25bd8515353
karankullar <jaskaran.kullar.13@ucl.ac.uk>
2015-10-12 16:18:59 +0100

    pre commenting, and pre non static methods

---
+++ PHAS3459/src/module2/ThreeVector.java
@@ -0,0 +1,41 @@
+package module2;
+public class ThreeVector {
+private double x;
+private double y;
+private double z;
+		
+	public ThreeVector(double xx, double yy, double zz){
+		x = xx;
+		y = yy;
+		z = zz;
+	}
+	
+	public double magnitude() {
+		return Math.sqrt(x*x + y*y + z*z); // returns magnitude of three vector 
+	}
+	
+	public ThreeVector unitVector (){
+		double mag = this.magnitude();
+		return new ThreeVector(x/mag, y/mag, z/mag);
+	}
+	public String toString(){
+		return "The normalised vector is ("+x+","+y+","+z+")";
+	}
+	
+	public static double scalarProduct(ThreeVector a, ThreeVector b){
+		return (a.x*b.x) + (a.y*b.y) + (a.z*b.z);
+	}
+	
+	public static ThreeVector vectorProduct (ThreeVector a, ThreeVector b){
+		return new ThreeVector (a.y*b.z - a.z*b.y, a.z*b.x - a.x*b.z, a.x*b.y - a.y*b.x);
+	}
+	
+	public static ThreeVector add(ThreeVector a, ThreeVector b){
+		return new ThreeVector (a.x+b.x, a.y+b.y, a.z +b.z);
+	}
+	
+	public static double angle(ThreeVector a, ThreeVector b){
+		return Math.acos(scalarProduct(a,b)/(a.magnitude()*b.magnitude()));
+	}
+
+}
