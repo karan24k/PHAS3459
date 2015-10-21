84285ae991291f1c2576396fe0aca68004169b63
karankullar <jaskaran.kullar.13@ucl.ac.uk>
2015-10-21 12:51:17 +0100

    done up to adding the character to the string

--- PHAS3459/src/module3/Alphabet.java
+++ PHAS3459/src/module3/Alphabet.java
@@ -1,9 +1,39 @@
 package module3;
 
+import java.util.Random;
+import java.lang.Character;
+import java.lang.Integer;
+import java.lang.StringBuilder;
+
 public class Alphabet {
 
-	public static void main(String[] args) {
+	private static StringBuilder str = new StringBuilder(500);
+	private static int sum = 0;
+	private static int letters = 0;
 
+	public static void main(String[] args) {
+		// Run loop 1000 times (undetermined no. of characters)
+		int i = 0;
+		while (i < 500) {
+
+			// Generate random char
+			char ranChar = randomCharacter();
+
+			// If char is letter or digit
+			if (Character.isLetterOrDigit(ranChar)) {
+
+				// Add to StringBuilder
+				str.append(ranChar);
+			}	
 	}
 
+	public static char randomCharacter() {
+
+		// Get random number between 0-127
+		int ran = new Random().nextInt(128);
+
+		// Cast to char and return
+		return (char) ran;
+
+	}
 }
