b7baa16a2e323fa5276500c20c9f347eff4346ab
karankullar <jaskaran.kullar.13@ucl.ac.uk>
2015-11-02 11:45:57 +0000

    final and commented

--- PHAS3459/src/module4/WordCounter.java
+++ PHAS3459/src/module4/WordCounter.java
@@ -8,8 +8,9 @@ public class WordCounter {
 
 	public static void main(String[] args)  {
 		
+		// Creates another BufferReader variable using brFromURL and prints the number of words using countsWordsInResource method.
 		try {
-			BufferedReader data = brFromURL("xjnxbjkasb");
+			BufferedReader data = brFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module4/module4_text.txt");
 			int words = countWordsInResource(data);
 			System.out.println("Number of words " + words);
 		}
@@ -20,6 +21,7 @@ public class WordCounter {
 		 
 	}
 
+	// Method to read URL and return buffered reader object
 	public static BufferedReader brFromURL(String urlName) throws IOException{
 		URL u = new URL(urlName);
 		InputStream is = u.openStream();
@@ -29,9 +31,11 @@ public class WordCounter {
 		return b;
 	}
 
+	// Counts the number of words in URL
 	private static int countWordsInResource(BufferedReader dataAsBR) throws IOException{
 		Scanner s = new Scanner(dataAsBR);
 		int countwords = 0;
+		// While there is a line to read, create a string, and count number of words
 		while (s.hasNext()){
 			s.next();
 			countwords ++;
