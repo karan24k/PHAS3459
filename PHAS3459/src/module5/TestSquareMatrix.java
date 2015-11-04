dc78677f68c2505933ecf2a506e85bac6b226314
karankullar <jaskaran.kullar.13@ucl.ac.uk>
2015-11-04 13:18:26 +0000

    FINAL TestSquareMatrix commented and tested exceptions

--- PHAS3459/src/module5/TestSquareMatrix.java
+++ PHAS3459/src/module5/TestSquareMatrix.java
@@ -3,6 +3,8 @@ package module5;
 public class TestSquareMatrix {
 
 	public static void main(String[] args) {
+		
+		// create matrices
 		SquareMatrix A = new SquareMatrix(new double[][] {{1,0,-2},{0,3,0},{2,0,1}});
 		SquareMatrix B = new SquareMatrix(new double[][] {{0,0,1},{0,1,0},{-1,0,1}});
 		SquareMatrix C = new SquareMatrix(new double[][] {{4,3},{3,2}});
@@ -10,17 +12,27 @@ public class TestSquareMatrix {
 		// testing toString System.out.println(A);
 		// testing unitMatrix System.out.println(SquareMatrix.unitMatrix(3));
 		
+		/*try adding, subtracting, multiplying and finding the commutator of matrices A and B, multiply C and D 
+		 * and test whether the outcome it is equal to the identity
+		 */
+		
 		try {
 			System.out.println("A+B =\n" + A.add(B));
 			System.out.println("A-B =\n" + A.subtract(B));
 			System.out.println("A x B =\n" + A.multiply(B));
 			System.out.println("B x A =\n" + B.multiply(A));
 			System.out.println("[A,B] =\n" + A.commutator(B));
-			System.out.println("C x D =\n" + C.multiply(D));
-			System.out.println("C x D = I? \n" + C.multiply(D).equals(SquareMatrix.unitMatrix(2)));
-		} catch (Exception e) {
+			SquareMatrix CD = C.multiply(D);
+			System.out.println("C x D =\n" + CD);
+			System.out.println("C x D = I? \n" + CD.equals(SquareMatrix.unitMatrix(2)));
+		} 
+		
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	// test must be square exception SquareMatrix E = new SquareMatrix(new double[][] {{1,0,-2},{0,3,0}});
+	// test same size exception SquareMatrix CD = B.multiply(D); 
 
 }
