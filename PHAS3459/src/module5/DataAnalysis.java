e0e21aae186401f2d615cfce5fc1575ae69c5549
zcapjss <zcapjss@ucl.ac.uk>
2015-11-09 18:03:06 +0000

    DataAnalysis - prints to screen, requires commenting

--- PHAS3459/src/module5/DataAnalysis.java
+++ PHAS3459/src/module5/DataAnalysis.java
@@ -10,17 +10,22 @@ import java.util.*;
 public class DataAnalysis {
 
 	public static void main(String[] args) {
-		ArrayList<DataPoint> data = new ArrayList<DataPoint> ();
-		
+		ArrayList<DataPoint> data = new ArrayList<DataPoint>();
+
 		try{
 			data = dataFromURL("http://www.hep.ucl.ac.uk/undergrad/3459/data/module5/module5-xy.txt");
 			double f2 = goodnessOfFit(new Theory(2),data);
 			double f4 = goodnessOfFit(new Theory(4),data);
-			
+
 			System.out.printf("Chi-squared value for y=x^2: "+"%f\n",f2);
 			System.out.printf("Chi-squared value for y=x^4: "+"%f\n",f4);
-			// Inline if statement to check which chi-squared value is bigger
-			System.out.println((f2>f4 ? "y=x^2" : "y=x^4")+" is a better fit for the measured data.");
+
+			if (f2>f4){
+				System.out.println("y=x^4 is a better fit for the data");
+			}
+			else{
+				System.out.println("y=x^2 is a better fit for the data");
+			}
 		}
 		catch(IOException e){
 			System.out.println(e.getMessage());
@@ -29,37 +34,39 @@ public class DataAnalysis {
 
 	}
 
-	public static ArrayList<DataPoint> dataFromURL(String url) throws IOException {
+	public static ArrayList<DataPoint> dataFromURL(String url) throws IOException, IllegalArgumentException {
 		ArrayList<DataPoint> dataSet = new ArrayList<DataPoint> ();
 
 		URL u = new URL(url);
 		InputStream is = u.openStream();
 		InputStreamReader isr = new InputStreamReader(is);
 		BufferedReader b = new BufferedReader(isr);
-		
+
 		String line;
-		line = b.readLine();
-		
-		while(line != null){
+
+		while((line = b.readLine())!= null){
 			String[] values = line.split("\\s+");
+			if (values.length != 3) {
+				throw new IllegalArgumentException("Input contains more than 3 values");
+			}
 			double [] numbers = new double [3];
-			
+
 			for (int i=0; i<values.length; i++) {
 				numbers [i] = Double.parseDouble(values[i]);
 			}
-			
+
 			dataSet.add(new DataPoint(numbers[0],numbers[1],numbers[2]));
 		}
-		
+
 		return dataSet;
 	}
-	
+
 	public static double goodnessOfFit(Theory t, ArrayList<DataPoint> data){
 		//initialise
 		double CHISQUARE = 0;
 		// for each datapoint in data from url
 		for (DataPoint p : data){
-			
+
 			// calculate theoretical y
 			double tY = t.y(p.getX());
 			// get measured y
