6650492753074a61090a29774721818096d6fc1b
zcapjss <zcapjss@CT212G21.ad.ucl.ac.uk>
2015-11-23 16:36:24 +0000

    FINAL - GoodnessOfFitCalculator

---
+++ PHAS3459/src/module6/GoodnessOfFitCalculator.java
@@ -0,0 +1,8 @@
+package module6;
+
+import java.util.Collection;
+
+public interface GoodnessOfFitCalculator {
+	// Define methods without implementation
+	double goodnessOfFit(Collection<DataPoint> data, Theory theory);
+}
