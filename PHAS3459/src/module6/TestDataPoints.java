8208a55094340eb0a50b2df3098b38e76e93f54b
zcapjss <zcapjss@CT212G21.ad.ucl.ac.uk>
2015-11-23 15:00:39 +0000

    TestDataPoints - done method for building collections, labelled data
    point not working

--- PHAS3459/src/module6/TestDataPoints.java
+++ PHAS3459/src/module6/TestDataPoints.java
@@ -6,6 +6,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collection;
 
 import module5.DataPoint;
 
@@ -16,8 +17,8 @@ public class TestDataPoints {
 
 	}
 
-	public static ArrayList<DataPoint> dataFromURL(String url) throws IOException {
-		ArrayList<DataPoint> dataSet = new ArrayList<DataPoint> ();
+	public static Collection<DataPoint> dataFromURL(String url) throws IOException {
+		Collection<DataPoint> dataSet = new ArrayList<DataPoint> ();
 
 		URL u = new URL(url);
 		InputStream is = u.openStream();
@@ -25,6 +26,36 @@ public class TestDataPoints {
 		BufferedReader br = new BufferedReader(isr);
 
 		String line;
-		
-		
+		// Read each line and extract data
+		while ((line = br.readLine()) != null) {
+
+			//split string up depending on spaces between text
+			String[] values = line.split("\\s+");
+
+			if(values.length == 3){
+				double [] numbers = new double [3];
+
+				//add data to arraylist
+				for (int i=0; i<values.length; i++) {
+					numbers [i] = Double.parseDouble(values[i]);
+				}
+
+				dataSet.add(new DataPoint(numbers[0],numbers[1],numbers[2]));
+			}
+			
+			else if(values.length == 4){
+				double [] numbers = new double [3];
+
+				//add data to arraylist
+				for (int i=0; i<values.length; i++) {
+					numbers [i] = Double.parseDouble(values[i]);
+				}
+				String label = values[3];
+				dataSet.add(new LabelledDataPoint(numbers[0],numbers[1], numbers[2], label));
+			}
+			return dataSet;
+
+
+		}
+	}
 }
