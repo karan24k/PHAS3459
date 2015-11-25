951a4403e196d9711253f336de254e5e91c2e25f
zcapjss <zcapjss@CT090103.ad.ucl.ac.uk>
2015-11-25 11:31:19 +0000

    FINAL ChiSquared - same as module 5 code for chisquared in DataAnalysis

--- PHAS3459/src/module6/ChiSquared.java
+++ PHAS3459/src/module6/ChiSquared.java
@@ -1,5 +1,23 @@
 package module6;
 
-public class ChiSquared implements GoodnessOfFitCalculator {
+import java.util.Collection;
 
+public class ChiSquared implements GoodnessOfFitCalculator {
+	public double goodnessOfFit(Collection<DataPoint> data, Theory t) {
+		//initialise
+				double CHISQUARE = 0;
+				// for each datapoint in data from url
+				for (DataPoint p : data){
+
+					// calculate theoretical y
+					double tY = t.y(p.getX());
+					// get measured y
+					double mY = p.getY();
+					// calculate chi squared numerator and denominator
+					double dY = Math.pow((mY-tY), 2);
+					double eYsquare = Math.pow(p.getEY(), 2);
+					CHISQUARE += dY/eYsquare;
+				}
+				return CHISQUARE;
+	}
 }
